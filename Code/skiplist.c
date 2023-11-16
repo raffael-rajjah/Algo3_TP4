08fe104092d3ad8966e24a52735dd8d12edf5fd2
raffael-rajjah <raffael.dwyyana@univ-tlse3.fr>
2023-11-16 14:24:05 +0100

    second useless commit

---
+++ Code/skiplist.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <assert.h>
+
+#include "skiplist.h"
+#include "rng.h"
+
+
+SkipList skiplist_create(int nblevels) {
+	(void)nblevels;
+	return NULL;
+}
+
+void skiplist_delete(SkipList d) {
+	(void)d;
+}
+
+SkipList skiplist_insert(SkipList d, int value) {
+	(void)value;
+	return d;
+}
